77dafe898451f836cdea533144cc26f5d22201d3
Chloe Kudryavtsev <toast@toast.cafe>
2019-12-03 13:29:50 -0500

    Fixup aoc19-1 tests
    Because of how testify outputs stuff, this makes more sense.

--- lib/1_test.go
+++ lib/1_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestFuelFromMass(t *testing.T) {
-	assert.Equal(t, FuelFromMass(12), 2, "they should be equal")
-	assert.Equal(t, FuelFromMass(14), 2, "they should be equal")
-	assert.Equal(t, FuelFromMass(1969), 654, "they should be equal")
-	assert.Equal(t, FuelFromMass(100756), 33583, "they should be equal")
+	assert.Equal(t, 2, FuelFromMass(12), "they should be equal")
+	assert.Equal(t, 2, FuelFromMass(14), "they should be equal")
+	assert.Equal(t, 654, FuelFromMass(1969), "they should be equal")
+	assert.Equal(t, 33583, FuelFromMass(100756), "they should be equal")
 }
 
 func TestRecursiveFuelFromMass(t *testing.T) {
-	assert.Equal(t, RecursiveFuelFromMass(14), 2, "they should be equal")
-	assert.Equal(t, RecursiveFuelFromMass(1969), 966, "they should be equal")
-	assert.Equal(t, RecursiveFuelFromMass(100756), 50346, "they should be equal")
+	assert.Equal(t, 2, RecursiveFuelFromMass(14), "they should be equal")
+	assert.Equal(t, 966, RecursiveFuelFromMass(1969), "they should be equal")
+	assert.Equal(t, 50346, RecursiveFuelFromMass(100756), "they should be equal")
 }
